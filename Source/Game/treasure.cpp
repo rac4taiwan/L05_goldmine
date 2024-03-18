74c99cdc81769757dc08bce4d3e964b2cad0fd25
rac4taiwan <sharron0826@gmail.com>
2024-03-18 20:22:09 +0800

    add project

---
+++ Source/Game/treasure.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include "mytreasure.h"
+
+using namespace game_framework;
+
+Treasure::Treasure() {
+
+}
+
+Treasure::~Treasure() {
+
+}
+
+double Treasure::Catch() {
+	return 0.9;
+}
+
+int Treasure::Score() {
+	return 0;
+}
+
+int Treasure::Time() {
+	return 0;
+}
